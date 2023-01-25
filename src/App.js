e6213a1c05609e23fbaed87caad959cacc799eb2
ABHISHEK SINGH <49355119+abhishek98as@users.noreply.github.com>
2023-01-26 02:20:16 +0530

    Changes in hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Navbar from './components/Navbar';
+import Categories from './Pages/Categories';
+import Home from './Pages/Home';
+
 
 const App = () => {
   return (
     <>
+    <Router>
+    <Navbar/>
+      <Routes>
+         <Route exact path ="/" element={<Home/>}/>
+         <Route  path ="/general" element={<Categories cat="general"/>}/>
+         <Route  path ="/Business" element={<Categories cat="Business"/>}/>
+         <Route  path ="/Entertaiment" element={<Categories cat="Entertaiment"/>}/>
+         <Route  path ="/Health" element={<Categories cat="Health"/>}/>
+         <Route  path ="/Science" element={<Categories cat="Science"/>}/>
+         <Route  path ="/Sports" element={<Categories cat="Sports"/>}/>
+         <Route  path ="/Technology" element={<Categories cat="Technology"/>}/>
 
-      <Navbar/>
+      </Routes>
+    </Router>
+
+      
 
       </>
   )
---
+++ src/Pages/Categories.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Categories = ({cat}) => {
+  return <div>{cat}</div>;
+  
+};
+
+export default Categories;
