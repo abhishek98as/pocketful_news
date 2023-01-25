d5edc3f61a0ab338b0f4a3f3e823def98b155a97
ABHISHEK SINGH <49355119+abhishek98as@users.noreply.github.com>
2023-01-26 03:01:29 +0530

    fetching news api

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Hero() {
+  return (
+    <div className="container-fluid bg-dark text-white d-flex justify-content-center align-items-center flex-column" style={{height:"50vh"}}>
+    <h2 style={{fontSize:"50px" , color:"orangered"}}>Pocketful News</h2>
+    <h5>THE WEBSITE BUILD WITH NEWS API</h5>
+    
+    </div>
+  );
+};
+
+export default Hero;
