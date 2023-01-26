e24d61205466c6172c6cfcc4afdd116a405bee85
ABHISHEK SINGH <49355119+abhishek98as@users.noreply.github.com>
2023-01-26 14:00:08 +0530

    loginsetup

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-// import FetchData from './Pages/FetchData';
 import Home from './Pages/Home';
 import FetchData from './components/FetchData';
+//import Fhome from './components/Fhome';
 
 
 
@@ -16,13 +16,14 @@ const App = () => {
       <Routes>
          <Route exact path ="/" element={<Home/>}/>
          <Route  path ="/general" element={<FetchData cat="general"/>}/>
-         <Route  path ="/general" element={<FetchData cat="general"/>}/>
          <Route  path ="/Business" element={<FetchData cat="Business"/>}/>
          <Route  path ="/Entertainment" element={<FetchData cat="Entertainment"/>}/>
          <Route  path ="/Health" element={<FetchData cat="Health"/>}/>
          <Route  path ="/Science" element={<FetchData cat="Science"/>}/>
          <Route  path ="/Sports" element={<FetchData cat="Sports"/>}/>
          <Route  path ="/Technology" element={<FetchData cat="Technology"/>}/>
+ 
+          
       </Routes>
 
       <Footer/>
